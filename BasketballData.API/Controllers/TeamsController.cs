b8478c94adce2ed46ee49f63b3c238efaca08d99
Botond Gorfol <botond.gorfol@gmail.com>
2020-10-22 20:14:41 +0200

    Init TeamsController

---
+++ BasketballData.API/Controllers/TeamsController.cs
@@ -0,0 +1,25 @@
+﻿using BasketballData.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BasketballData.API.Controllers
+{
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly IBasketballDataRepository _basketballDataRepository;
+        
+        public TeamsController(IBasketballDataRepository basketballDataRepository)
+        {
+            _basketballDataRepository = basketballDataRepository ?? throw new ArgumentNullException(nameof(basketballDataRepository));
+        }
+        
+        [HttpGet("api/teams")]
+        public IActionResult GetTeams()
+        {
+            var teamsFromRepo = _basketballDataRepository.GetTeams();
+
+            return new JsonResult(teamsFromRepo);
+        }
+    }
+}
